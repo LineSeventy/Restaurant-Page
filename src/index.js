3294516f0d4e81a6adf353ce67f5a03399174e94
LineSeventy <169619934+LineSeventy@users.noreply.github.com>
2024-11-19 05:28:54 +0800

    test

--- src/index.js
+++ src/index.js
@@ -1,4 +1,13 @@
 import "./style.css";
 
+const displaySystem = () => {
+    const content = document.querySelector("#content");
+    const img = document.createElement("image");
+    const details = document.createElement("div");
+    
+    content.appendChild(img);
+    img.appendChild(details);
+}
 
+displaySystem();
 console.log("This is a test")
