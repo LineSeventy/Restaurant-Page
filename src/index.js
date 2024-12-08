92bc4cf67d02e81bac50205061872d8555876490
LineSeventy <169619934+LineSeventy@users.noreply.github.com>
2024-12-08 17:34:10 +0800

    3

--- src/index.js
+++ src/index.js
@@ -1,10 +1,6 @@
 import "./style.css";
 import "./home.css";
-<<<<<<< HEAD
-import "./menu.css";
-=======
 import "./home.css";
->>>>>>> main
 import "./about.css";;
 import {home} from "./homeTab";
 import {menuTab} from "./menuTab";
